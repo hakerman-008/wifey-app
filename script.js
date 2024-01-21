d94b42edd2d819ba0c7fda5dd99266925184de64
kshitiz-008 <142625808+kshitiz-008@users.noreply.github.com>
2024-01-21 06:52:22 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const videoElement = document.getElementById('wifey-video');
 async function toggleVideo() {
   const playButton = document.getElementById('play-button');
 
-  if (isVideoPaused) {
-    videoElement.play();
+  try {
+    await videoElement.play();
     playButton.innerText = 'Pause Video';
-  } else {
-    videoElement.pause();
-    playButton.innerText = 'Play Video';
+  } catch (error) {
+    // Autoplay was prevented, request user interaction
+    console.error('Autoplay was prevented. Please interact with the video to play.');
   }
 
   isVideoPaused = !isVideoPaused;
@@ -21,25 +21,19 @@ async function getRandomVideo() {
     const rawResponse = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ apikey: "$shoti-1hecj3cvm6r1mf91948" })
+      body: JSON.stringify({ apikey: "$shoti-1hecj3cvm6r1mf91948" }),
     });
     const content = await rawResponse.json();
 
-    
     videoElement.src = content.data.url;
     videoElement.play();
-
-    
     document.getElementById('play-button').innerText = 'Pause Video';
-
-   
     isVideoPaused = false;
   } catch (e) {
     console.error("Error fetching Shoti video", e);
   }
 }
-
 
 getRandomVideo();
